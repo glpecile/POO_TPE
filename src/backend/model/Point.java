17fc802de6bd738d1a980eb1f48019c0a7f94e8f
Julian Arce <juarce@itba.edu.ar>
2020-07-05 21:30:28 -0300

    Se agrega el método draw a Line

--- src/backend/model/Point.java
+++ src/backend/model/Point.java
@@ -18,19 +18,14 @@ public class Point implements Movable{
     }
 
     @Override
-    public String toString() {
-        return String.format("{%.2f , %.2f}", x, y);
-    }
-
-    @Override
     public void move(double moveInX, double moveInY) {
         x += moveInX;
         y += moveInY;
     }
+
     public double distanceTo(Point p){
         return Math.sqrt(Math.pow(p.getX() - x, 2) + Math.pow(p.getY() - y, 2));
     }
-
     public double horizontalDistanceTo(Point p) {
         return Math.abs(p.getX() - x);
     }
@@ -41,5 +36,10 @@ public class Point implements Movable{
 
     public Point substract(Point other){
         return new Point(getX()-other.getX(), getY()-other.getY());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{%.2f , %.2f}", x, y);
     }
 }
